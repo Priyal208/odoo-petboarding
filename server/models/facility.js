56b6c4c6d91ca2774a81c2c0c13a3edb3b96366e
Prajapati Megh Kishorbhai <prajapatimeghkishorbhai@Meghs-MacBook-Pro.local>
2024-06-29 15:46:31 +0530

    Added booking routes and facility and pets routing

--- server/models/facility.js
+++ server/models/facility.js
@@ -6,14 +6,6 @@ const facilitySchema = new mongoose.Schema({
   name: { type: String, required: true },
   capacity: { type: Number, required: true },
   currentCapacity: {type:Number, default: 0},
-  bookings: [
-    {
-      startDate: Date,
-      endDate: Date,
-      bookedBy: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
-      petId: { type: mongoose.Schema.Types.ObjectId, ref: "Pet" },
-    },
-  ],
 });
 
 module.exports = mongoose.model("Facility", facilitySchema);
